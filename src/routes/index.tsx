8c2f42cc866220ec8d381b7fbbc8a2a278550478
mayun19 <mutiara.ayun19@gmail.com>
2022-01-21 20:19:41 +0700

    Muti - tempalte Detail Product responsive done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,27 +5,29 @@ import Account from "../pages/account";
 import Dashboard from "../pages/account/dashboard";
 import Transaction from "../pages/account/transaction";
 import Detail from "../pages/account/transaction/detail";
-import Login from "../pages/login";
-import Register from "../pages/register";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import ProductDetail from "../pages/ProductDetail";
 
 const Index = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="account" element={<Account />}>
-            <Route index element={<Navigate to="dashboard" />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="transaction" element={<Transaction />} />
-            <Route path="transaction/:id" element={<Detail />} />
-          </Route>
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />}>
+					<Route index element={<Home />} />
+					<Route path="product" element={<ProductDetail />} />
+					<Route path="account" element={<Account />}>
+						<Route index element={<Navigate to="dashboard" />} />
+						<Route path="dashboard" element={<Dashboard />} />
+						<Route path="transaction" element={<Transaction />} />
+						<Route path="transaction/:id" element={<Detail />} />
+					</Route>
+				</Route>
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default Index;
