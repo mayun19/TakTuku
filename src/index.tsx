5416cbfd344f2e0c6b032571b2476b8da5714241
Chandra Wijaya <chandraw8999@gmail.com>
2022-01-25 13:50:24 +0700

    fixing

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "./assets/scss/index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
-import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
 import Routes from "./routes";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
@@ -15,12 +13,12 @@ import axios from "axios";
 axios.defaults.baseURL = "http://54.151.226.92:8080/";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<Routes />
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
